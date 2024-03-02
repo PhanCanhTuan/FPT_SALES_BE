70254014debf2e4b199f180d7e31eee569b0779f
datntse150392 <149555351+PhanCanhTuan@users.noreply.github.com>
2024-03-02 18:05:54 +0700

    Revert "Update new API"
    This reverts commit 1ec9ad1e2ce0875ab120912664f05aff89a049aa.

--- models/item.model.js
+++ models/item.model.js
@@ -4,35 +4,26 @@ module.exports = model;
 
 function model(sequelize) {
   const attributes = {
-    opening_id: {
+    item_id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       autoIncrement: true,
       primaryKey: true,
     },
-    property_id: {
+    icon: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    value: {
       type: DataTypes.INTEGER,
       allowNull: false,
     },
-    start_date: {
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-    end_date: {
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-    status: {
-      type: DataTypes.STRING(20),
-      allowNull: false,
-    },
   };
-
   const options = {
     freezeTableName: true,
     // Không thêm các thuộc tính thời gian (updatedAt, createdAt)
     timestamps: false,
   };
 
-  return sequelize.define("OpeningForSales", attributes, options);
+  return sequelize.define("Items", attributes, options);
 }
