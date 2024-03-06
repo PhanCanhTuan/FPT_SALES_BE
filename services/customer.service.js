7dd8dd520c3b2d7520fe37a461b1774829ae544f
PhanCanhTuan <tuanpcse170186@fpt.edu.vn>
2024-03-06 08:17:23 +0700

    [+] update customer

--- services/customer.service.js
+++ services/customer.service.js
@@ -21,7 +21,37 @@ const getBookingByCustomer = async (CustomerId) => {
   };
 };
 
+const updateCustomer = async (userId, {
+  fullName,
+  email,
+  phoneNumber,
+  address
+}) => {
+  let customer = await db.CustomerModel.findOne({
+    where: { UserId: userId },
+  });
+
+  if(!customer){
+    return {
+      status: 404,
+      message: "Customer not found"
+    }
+  }
+
+  customer.FullName= fullName ?? customer.FullName,
+  customer.Email= email ?? customer.Email,
+  customer.PhoneNumber= phoneNumber ?? customer.PhoneNumber,
+  customer.Address= address ?? customer.Address,
+
+  await customer.save();
+  return {
+    status: 200,
+    message: "Update customer successfully"
+  }
+}
+
 module.exports = {
   getAllCustomer,
   getBookingByCustomer,
+  updateCustomer
 };
